9fd11919657d27f614ccee36a16c99aae8ac48d6
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-21 14:13:32 +0100

    ADD random numbers generation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-console.log("JS linked");
-
 /*
 Descrizione:
 Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
@@ -14,3 +12,19 @@ Individuate gli elementi di cui avete bisogno per realizzare il programma.
 Bonus:
 Invece di usare prompt e allerte usate inputs ed elementi della dom per mostrare a schermo il risultato.
 */
+
+// Show the numbers on the page and start a 30s timer
+// At the end the numbers have to disappear
+// The user inserts 5 numbers and I have to check how many and which ones the user remembers correctly
+
+// Create a 5 unique numbers array
+const numbersToMemorize = [];
+while (numbersToMemorize.length < 5) {
+  // Numbers between 0 and 100 (both included) will be generated
+  const singleNumberToMemorize = Math.floor(Math.random() * 101);
+  if (!numbersToMemorize.includes(singleNumberToMemorize)) {
+    numbersToMemorize.push(singleNumberToMemorize);
+  }
+}
+
+console.log(numbersToMemorize);
